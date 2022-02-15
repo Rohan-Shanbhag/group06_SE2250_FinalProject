9bb443b2d7a3e0e223d8bdd8f785eb10011ba174
Rohan Shanbhag <rohanshanbhag99@gmail.com>
2022-02-15 12:22:28 -0500

    completed phase 1 implementation of level 1 prototype

---
+++ Assets/Scripts/CharacterOneController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterOneController : MonoBehaviour
+{
+    public static CharacterOneController instance;
+
+    [HideInInspector]
+    public bool isAbleToMove = true;
+
+    public Rigidbody2D rigidBody;
+    public Transform gunHoldingArm;
+    private Vector2 movingInput;
+    public float movingSpeed;
+    public SpriteRenderer bodySpriteRenderer;
+    private float activeMovingSpeed;
+
+    private void Awake()
+    {
+        instance = this;
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        activeMovingSpeed = movingSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isAbleToMove)
+        {
+            movingInput.x = Input.GetAxisRaw("Horizontal");
+            movingInput.y = Input.GetAxisRaw("Vertical");
+            movingInput.Normalize();
+
+            rigidBody.velocity = movingInput * activeMovingSpeed;
+
+        } else
+        {
+            rigidBody.velocity = Vector2.zero;
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public Rigidbody2D rigidBody;
+
+    [Header("Variables")]
+    public SpriteRenderer bodySpriteRenderer;
+
+    public int health = 150;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidBody.velocity = Vector2.zero;
+    }
+
+}
